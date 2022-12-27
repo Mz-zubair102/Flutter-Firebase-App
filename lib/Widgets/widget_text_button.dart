5f43a190e8392a0c48cf81f2d370afabb67a365c
Muhammad Zubair <mz.zubair102@gmail.com>
2022-12-27 18:49:14 +0500

    firstcommit

---
+++ lib/Widgets/widget_text_button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class widgetTextButton extends StatelessWidget {
+  final VoidCallback? onpressed;
+  final String btnname;
+  const widgetTextButton({Key? key,this.onpressed, required this.btnname}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+        onPressed: onpressed,
+        style: TextButton.styleFrom(
+            primary: Colors.white,
+            elevation: 15,
+            backgroundColor: Colors.cyan,
+            shadowColor: Colors.red,
+            shape: BeveledRectangleBorder(
+              borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(10),
+                  bottomLeft: Radius.circular(10)),
+            )),
+        child: Text(
+          btnname,
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 30,
+            fontStyle: FontStyle.italic,
+          ),
+        ));
+  }
+}
